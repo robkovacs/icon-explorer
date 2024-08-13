41a528f1a83b82357ab3cf814a7f34431a072a33
Rob Kovacs <rkovacs@justworks.com>
2024-08-13 00:09:32 -0400

    Run Prettier, add Prettier config

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,11 @@
     let currentQuery = input.value;
 
     async function getIcons() {
-        const response = await fetch('assets/js/icons.json');
-        icons = await response.json(); 
+        const response = await fetch("assets/js/icons.json");
+        icons = await response.json();
     }
-    
-    async function populateGallery(data = '') {
+
+    async function populateGallery(data = "") {
         await getIcons();
         if (!data) {
             data = icons;
@@ -18,37 +18,40 @@
         const gallery = document.querySelector("#gallery");
         const inspector = document.querySelector("#inspector");
         if (gallery.innerHTML) {
-            gallery.innerHTML = '';
+            gallery.innerHTML = "";
             inspector.style.display = "block";
-            inspector.innerHTML = '<div class="empty-state">Choose an icon to see its details here</div>';
+            inspector.innerHTML =
+                '<div class="empty-state">Choose an icon to see its details here</div>';
         }
 
-        if (JSON.stringify(data) === '{}') {
-            gallery.innerHTML = '<div class="empty-state">No matching icons found</div>';
+        if (JSON.stringify(data) === "{}") {
+            gallery.innerHTML =
+                '<div class="empty-state">No matching icons found</div>';
             inspector.style.display = "none";
         }
-        
-        const iconSummaryTemplate = document.querySelector("#icon-summary-template");
+
+        const iconSummaryTemplate = document.querySelector(
+            "#icon-summary-template"
+        );
 
         // Sort the JSON alphabetically by key name, just in case it's out of order
-        data = Object.keys(data).sort(sortIcons).reduce(
-            (obj, key) => { 
-              obj[key] = data[key]; 
-              return obj;
-            }, 
-            {}
-        );
+        data = Object.keys(data)
+            .sort(sortIcons)
+            .reduce((obj, key) => {
+                obj[key] = data[key];
+                return obj;
+            }, {});
 
         let count = Object.keys(data).length;
         let countText = count + " matching icon";
-        document.querySelector('#icon-count').textContent = '';
+        document.querySelector("#icon-count").textContent = "";
 
         if (count !== 1) {
             countText += "s";
         }
 
         if (count > 0) {
-            document.querySelector('#icon-count').textContent = countText;
+            document.querySelector("#icon-count").textContent = countText;
         }
 
         for (let key in data) {
@@ -57,26 +60,34 @@
             let iconDescription = data[key].description;
             let isDeprecated = data[key].isDeprecated;
             let isNew = data[key].isNew;
-            
-            let iconSummary = iconSummaryTemplate.content.cloneNode(true);
-            let button = iconSummary.querySelector('button');
-            let iconContainer = iconSummary.querySelector('figure > div');
-            let figcaption = iconSummary.querySelector('figcaption');
 
-            button.addEventListener('click', function(e) {    
-                const currentButton = document.querySelector('button.current')
+            let iconSummary = iconSummaryTemplate.content.cloneNode(true);
+            let button = iconSummary.querySelector("button");
+            let iconContainer = iconSummary.querySelector("figure > div");
+            let figcaption = iconSummary.querySelector("figcaption");
+
+            button.addEventListener("click", function (e) {
+                const currentButton = document.querySelector("button.current");
                 if (currentButton) {
-                    currentButton.classList.toggle('current');
+                    currentButton.classList.toggle("current");
                 }
-                e.currentTarget.classList.toggle('current');
-                showIconDetails(iconName, iconTags, iconDescription, isDeprecated, isNew);
+                e.currentTarget.classList.toggle("current");
+                showIconDetails(
+                    iconName,
+                    iconTags,
+                    iconDescription,
+                    isDeprecated,
+                    isNew
+                );
             });
-            button.setAttribute('aria-label', iconName);
+            button.setAttribute("aria-label", iconName);
 
             // Use fetch() to get the actual SVG code (so that maybe one day we can have a color picker)
-            fetch('assets/img/icons/' + iconName + '.svg')
+            fetch("assets/img/icons/" + iconName + ".svg")
                 .then((response) => response.text())
-                .then((svg) => { iconContainer.innerHTML = svg });
+                .then((svg) => {
+                    iconContainer.innerHTML = svg;
+                });
 
             if (isDeprecated) {
                 figcaption.innerHTML = '<span class="deprecated-dot"></span>';
@@ -88,9 +99,9 @@
 
             gallery.appendChild(iconSummary);
         }
-    };
+    }
 
-    async function filterIcons(query = '', hideDeprecated = false) {
+    async function filterIcons(query = "", hideDeprecated = false) {
         await getIcons();
         let matchingIcons = {};
         if (!query) {
@@ -98,7 +109,7 @@
         }
 
         const regex = new RegExp(escapeRegex(query.toLowerCase().trim()));
-        
+
         for (let key in icons) {
             let iconName = key;
             let iconTags = icons[key].tags;
@@ -109,15 +120,15 @@
             }
 
             if (iconName.toLowerCase().search(regex) !== -1) {
-               matchingIcons[key] = icons[key];
-               continue;
+                matchingIcons[key] = icons[key];
+                continue;
             }
 
             for (let i = 0; i < iconTags.length; i++) {
                 if (iconTags[i].toLowerCase().search(regex) !== -1) {
                     matchingIcons[key] = icons[key];
                     continue;
-                }     
+                }
             }
 
             if (iconDescription.toLowerCase().search(regex) !== -1) {
@@ -131,36 +142,42 @@
 
     function showIconDetails(name, tags, description, isDeprecated, isNew) {
         const inspector = document.querySelector("#inspector");
-        
-        const iconDetailTemplate = document.querySelector("#icon-detail-template");
-        
-        let iconDetail = iconDetailTemplate.content.cloneNode(true);
-        let container = iconDetail.querySelector('.icon-detail');
-        let icon = iconDetail.querySelector('figure > div');
-        let figcaption = iconDetail.querySelector('figcaption');
-        let ul = iconDetail.querySelector('ul');
-        let p = iconDetail.querySelector('p');
 
-        fetch('assets/img/icons/' + name + '.svg')
+        const iconDetailTemplate = document.querySelector(
+            "#icon-detail-template"
+        );
+
+        let iconDetail = iconDetailTemplate.content.cloneNode(true);
+        let container = iconDetail.querySelector(".icon-detail");
+        let icon = iconDetail.querySelector("figure > div");
+        let figcaption = iconDetail.querySelector("figcaption");
+        let ul = iconDetail.querySelector("ul");
+        let p = iconDetail.querySelector("p");
+
+        fetch("assets/img/icons/" + name + ".svg")
             .then((response) => response.text())
-            .then((svg) => { icon.innerHTML = svg });
+            .then((svg) => {
+                icon.innerHTML = svg;
+            });
 
         figcaption.innerHTML = name;
 
         if (isDeprecated) {
-            figcaption.innerHTML += '<span class="deprecated-badge">Deprecated</span>'
+            figcaption.innerHTML +=
+                '<span class="deprecated-badge">Deprecated</span>';
         } else if (isNew) {
-            figcaption.innerHTML += '<span class="new-badge">New</span>'
+            figcaption.innerHTML += '<span class="new-badge">New</span>';
         }
 
         if (tags.length > 0) {
-            const iconTagTemplate = document.querySelector("#icon-tag-template");
+            const iconTagTemplate =
+                document.querySelector("#icon-tag-template");
             for (let i in tags) {
                 let iconTag = iconTagTemplate.content.cloneNode(true);
-                let button = iconTag.querySelector('button');
+                let button = iconTag.querySelector("button");
                 let tagName = tags[i];
                 button.textContent = tagName;
-                button.addEventListener('click', function(e) {
+                button.addEventListener("click", function (e) {
                     input.value = tagName;
                     filterIcons(tagName);
                 });
@@ -181,10 +198,10 @@
         }
 
         inspector.appendChild(iconDetail);
-    };
+    }
 
     function sortIcons(a, b) {
-        a = a.split("-"), b = b.split("-");
+        (a = a.split("-")), (b = b.split("-"));
 
         for (let i = 0; i <= Math.min(a.length, b.length); i++) {
             if (a[i] > b[i]) {
@@ -212,21 +229,25 @@
             const args = arguments;
             clearTimeout(debounceTimer);
             debounceTimer = setTimeout(() => func.apply(context, args), delay);
-        }
+        };
     }
 
-    input.addEventListener('keyup', debounce(function(e) {
-        if (this.value !== currentQuery) {
-            const toggleState = deprecatedToggle.getAttribute('aria-checked') === 'true'
-            currentQuery = this.value;
-            filterIcons(this.value, toggleState);
-        }
-    }, 250));
+    input.addEventListener(
+        "keyup",
+        debounce(function (e) {
+            if (this.value !== currentQuery) {
+                const toggleState =
+                    deprecatedToggle.getAttribute("aria-checked") === "true";
+                currentQuery = this.value;
+                filterIcons(this.value, toggleState);
+            }
+        }, 250)
+    );
 
-    deprecatedToggle.addEventListener('click', function(e) {
-        const currentState = this.getAttribute('aria-checked') === 'true';
+    deprecatedToggle.addEventListener("click", function (e) {
+        const currentState = this.getAttribute("aria-checked") === "true";
         const newState = String(!currentState);
-        this.setAttribute('aria-checked', newState);
+        this.setAttribute("aria-checked", newState);
         filterIcons(input.value, !currentState);
     });
 
